e7a65dca83b791931903b9f5bae82e21ebf84c10
Daniil Isaverdov <daniil.isaverdov@gmail.com>
2026-09-10 23:49:46 +0300

    feat: add health endpoint with chi router

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/mrDisa/Raspy/backend/internal/handler"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/health", handler.Health)
+
+	log.Fatal(http.ListenAndServe(":3000", r))
+	log.Println("server started on :3000")
+}
