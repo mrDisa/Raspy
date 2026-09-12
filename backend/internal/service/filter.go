64ff035f11c79237a7d11fe3d67bd5b2d2e97fee
Daniil Isaverdov <daniil.isaverdov@gmail.com>
2026-09-12 15:18:44 +0300

    feat: subgroup filtering in schedule service, add postgres via docker-compose

---
+++ backend/internal/service/filter.go
@@ -0,0 +1,13 @@
+package service
+
+import "github.com/mrDisa/Raspy/backend/internal/model"
+
+func filterBySubgroup(lessons []model.Lesson, subgroup model.Subgroup) []model.Lesson {
+	filteredLessons := make([]model.Lesson, 0)
+    for _, lesson := range lessons {
+		if lesson.Subgroup == model.SubgroupAll || lesson.Subgroup == subgroup {
+			filteredLessons = append(filteredLessons, lesson)
+		}
+	}
+	return filteredLessons
+}
--- backend/internal/service/schedule.go
+++ backend/internal/service/schedule.go
@@ -15,23 +15,60 @@ type ScheduleService struct {
     provider ScheduleProvider
 }
 
-func NewScheduleService(provider ScheduleProvider) ScheduleService {
-    return ScheduleService{
+func NewScheduleService(provider ScheduleProvider) *ScheduleService {
+    return &ScheduleService{
 		provider: provider,
 	}
 }
 
-func (s *ScheduleService) GetTodaySchedule(group string) (model.ScheduleDay, error) {
-	today := time.Now()
-	schedule, err := s.provider.GetSchedule(group, nil)
+func (s *ScheduleService) getFilteredSchedule(group string, subgroup model.Subgroup,  startDate *time.Time) ([]model.ScheduleDay, error) {
+	schedule, err := s.provider.GetSchedule(group, startDate)
 	if err != nil {
-		return model.ScheduleDay{}, fmt.Errorf("failed to get schedule: %v", err)
+		return []model.ScheduleDay{}, fmt.Errorf("failed to get schedule: %w", err)
 	}
-	dateToday := today.Format("2006-01-02")
+	for i := range schedule {
+		schedule[i].List = filterBySubgroup(schedule[i].List, subgroup)
+	}
+	return schedule, err
+}
+
+func (s *ScheduleService) getScheduleForDate(group string, subgroup model.Subgroup, date time.Time) (model.ScheduleDay, error) {
+	schedule, err := s.getFilteredSchedule(group, subgroup, nil)
+	if err != nil {
+		return model.ScheduleDay{}, fmt.Errorf("failed to get schedule: %w", err)
+	}
+
+	targetDate := date.Format("2006-01-02")
 	for _, day := range schedule {
-		if day.Date == dateToday {
+		if day.Date == targetDate {
 			return day, nil
 		}
 	}
-	return  model.ScheduleDay{}, fmt.Errorf("schedule for today not found")
+	return  model.ScheduleDay{}, fmt.Errorf("schedule for %s not found", targetDate)
+}
+
+func (s *ScheduleService) GetTodaySchedule(group string, subgroup model.Subgroup) (model.ScheduleDay, error) {
+	return s.getScheduleForDate(group, subgroup, time.Now())
+}
+func (s *ScheduleService) GetTomorrowSchedule(group string, subgroup model.Subgroup) (model.ScheduleDay, error) {
+	return s.getScheduleForDate(group, subgroup, time.Now().AddDate(0, 0, 1))
+}
+
+func (s *ScheduleService) GetNextWeekSchedule(group string, subgroup model.Subgroup) ([]model.ScheduleDay, error) {
+	weekday := int(time.Now().Weekday())
+	if weekday == 0 {
+		weekday = 7
+	}
+	daysUntilNextMonday := 8 - weekday
+	rawDate := time.Now().AddDate(0, 0, daysUntilNextMonday)
+
+	loc, err := time.LoadLocation(("Europe/Moscow"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to load timezone: %w", err)
+	}
+
+	date := time.Date(rawDate.Year(), rawDate.Month(), rawDate.Day(), 0, 0, 0, 0, loc)
+
+	fmt.Println(date)
+	return s.getFilteredSchedule(group,subgroup, &date)
 }
